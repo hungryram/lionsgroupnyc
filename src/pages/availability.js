1a79624d747ddb7fab0f55eaf371ccac2fa43947
Ram Dettmer <ramdettmer751@gmail.com>
2022-07-08 19:52:55 -0700

    add script

--- src/pages/availability.js
+++ src/pages/availability.js
@@ -1,12 +1,31 @@
 import * as React from "react"
 import Layout from "../components/global/Layout"
+import AvailabilityScript from "../components/templates/AvailabilityScript"
+import { Script } from "gatsby"
 
 export default function Availability() {
     return (
         <>
-        <Layout>
-            
-        </Layout>
+            <Layout>
+                <div className="section">
+                    <div className="container">
+                        <div
+                            dangerouslySetInnerHTML={{
+                                __html: `<script type='text/javascript' charset='utf-8'>
+                            document.write(unescape("%3Cscript src='" + (('https:' == document.location.protocol) ? 'https:' : 'http:') + "//lionsgroup.appfolio.com/javascripts/listing.js' type='text/javascript'%3E%3C/script%3E"));
+                          </script><script type='text/javascript' charset='utf-8'>
+                          Appfolio.Listing({
+                            hostUrl: 'lionsgroup.appfolio.com',
+                            themeColor: '#676767',
+                            height: '500px',
+                            width: '100%',
+                            defaultOrder: 'date_posted'
+                          });
+                        </script>` }}
+                        />
+                    </div>
+                </div>
+            </Layout>
         </>
     )
 }
